0d51460cf488e230452a1cf655b160dfa82aba02
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-24 16:58:18 +0200

    blabla

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -4,8 +4,11 @@
     
     <div id="background_im">
         <div id="background_op">
+            
         <img src="public/images/logo_violet.png" id="lviolet">
+        <div id="lbout">
         <button id="btn-1">Découvrez nos produits</button>
+        </div>
         </div>
         
     </div>
