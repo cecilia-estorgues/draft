8ce31283cb4777a4e956c06d4ea7f66b88bb54f6
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-04-24 14:36:11 +0200

    base

---
+++ app/view/common/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <ul>
+        <li><a href="">Nos produits</a></li>
+        <li><a href="">Actualités</a></li>
+        <li><a href="">Jeu</a></li>
+        <li><a href="">A propos de nous</a></li>
+    </ul>
+</footer>
+</body>
+</html>
---
+++ app/view/common/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<header>
+    <link rel="stylesheet" href="general.css">
+</header>
