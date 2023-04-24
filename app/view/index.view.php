aa41eb1632e1f05d463b6ebfbdf59bdc6410353d
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-24 17:26:36 +0200

    bloup

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -4,16 +4,20 @@
     
     <div id="background_im">
         <div id="background_op">
-            
+        <div id="lvioleto">
         <img src="public/images/logo_violet.png" id="lviolet">
+        </div>
         <div id="lbout">
-        <button id="btn-1">Découvrez nos produits</button>
+        <a href="#"> <button id="btn-1">Découvrez nos produits</button> </a>
         </div>
         </div>
-        
     </div>
     </section>
     <section>
+    <div id="background_im2">
+
+    </div>
+    
 
     </section>
 </main>
