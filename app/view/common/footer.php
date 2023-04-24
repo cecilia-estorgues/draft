0aad0d2b90cb3858fe06ff89f16bf58bea319c5b
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-04-24 15:41:49 +0200

    abel_24/04

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -5,6 +5,18 @@
         <li><a href="">Jeu</a></li>
         <li><a href="">A propos de nous</a></li>
     </ul>
+    <ul>
+        <li>
+            <ul>
+                <li><a href=""><img src="" alt="Twitter"></a></li>
+                <li><a href=""><img src="" alt="Facebook"></a></li>
+                <li><a href=""><img src="" alt="Instagram"></a></li>
+                <li><a href=""><img src="" alt="Tiktok"></a></li>
+            </ul>
+        </li>
+        <li>draft.brasserie@gmail.com</li>
+        <li>06 06 06 06 06</li>
+    </ul>
 </footer>
 </body>
 </html>
