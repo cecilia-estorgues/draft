18d182610f4f453a5489d379867499e0a3f9bcf6
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-04-25 15:12:19 +0200

    abel

---
+++ verification.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="public/css/check.css">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="bière, brasserie, locale, jeune, dynamique, street art">
+    <meta name="author" content="DRAFT">
+    <meta name="description" content="Site officiel de Draft, une brasserie jeune, dynamique et locale qui a pour but d'encourager les gens laisser s'exprimer leur créativité">
+</head>
+<body>
+<main>
+    <img src="public/images/Logo/logo_violet.png">
+    <h2>Dernière épreuve avant de nous rejoindre !</h2>
+    <h4>Avez vous 18ans ?</h4>
+    <a href="index.php" id="oui"><div>Oui</div></a>
+    <a href="refuse.php" id="non"><div>Non</div></a>
+</main>
+</body>    
+</html>
