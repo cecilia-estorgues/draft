ccdbf887a0218f4428efaba1aa7c6b8ceef70135
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-04-25 16:42:32 +0200

    abel

---
+++ refuse.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="public/css/refuse.css">
+    <title>Accès refusé</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="bière, brasserie, locale, jeune, dynamique, street art">
+    <meta name="author" content="DRAFT">
+    <meta name="description" content="Site officiel de Draft, une brasserie jeune, dynamique et locale qui a pour but d'encourager les gens laisser s'exprimer leur créativité">
+</head>
+<body>
+<main>
+<img src="public/images/Logo/lgo_bgnoir.png">
+<h2>Accès refusé</h2>
+<div><h4>Désolé mais tu n'as pas encore atteint l'âge requis pour accéder à notre site web. 
+Mais t'en fais pas, tu peux quand même chercher des infos sur <a href="http://www.responsibledrinking.org/?lang=fr">comment consommer de façon responsable.</a></h4></div>
+</main>
+</body>
