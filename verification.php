2fa4fcf791960f5a8aaa54118e5fab462361623b
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-04-25 15:43:06 +0200

    abel

--- verification.php
+++ verification.php
@@ -11,7 +11,7 @@
 <main>
     <img src="public/images/Logo/logo_violet.png">
     <h2>Dernière épreuve avant de nous rejoindre !</h2>
-    <h4>Avez vous 18ans ?</h4>
+    <h4>Avez vous 18 ans ?</h4>
     <a href="index.php" id="oui"><div>Oui</div></a>
     <a href="refuse.php" id="non"><div>Non</div></a>
 </main>
