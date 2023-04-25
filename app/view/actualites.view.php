a39f81af066dee4cd44d5554f6a01467631c6fea
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-25 17:40:49 +0200

    de

--- app/view/actualites.view.php
+++ app/view/actualites.view.php
@@ -5,14 +5,16 @@
 <section>
 <div id="title"><h1>NOTRE ACTUALITÉ</h1></div>
 
-<div id="contain1">
+<section id="contain2">
+    <div id="tit">
 <div id="gauche1">
 <img src="public/images/background/tache_jaune.png">
 </div>
 <div id="droite1"><img src="public/images/mockup/butineuse_vmaxou.png">
 </div>
 </div>
-
+</div>
+</section>
 </section>
 
 
