ae46112933c12de00c0d648fd180595ae2eed9d6
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-26 09:34:42 +0200

    toutouyoutou

--- app/view/actualites.view.php
+++ app/view/actualites.view.php
@@ -9,12 +9,33 @@
     <div id="tit">
 <div id="gauche1">
 <img src="public/images/background/tache_jaune.png">
+<p>dad</p>
 </div>
 <div id="droite1"><img src="public/images/mockup/butineuse_vmaxou.png">
 </div>
 </div>
 </div>
 </section>
+
+
+
+
+<section id="contain3">
+    <div id="tit2">
+<div id="gauche2">
+<h2>Festival de la bière</h2>
+<p>Le 7 juin </br> prochain
+</p>
+</div>
+<div id="droite2"><img rc="public/images/background/tache_orange.png">
+</div>
+</div>
+</div>
+</section>
+
+
+
+
 </section>
 
 
