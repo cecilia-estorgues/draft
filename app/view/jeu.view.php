46cd25395b1e1f2474a0d2698c7b0a9e02606347
cecilia-estorgues <cecilia.estorgues@gmail.com>
2023-04-26 16:30:50 +0200

    suite page jeu
    fait par catarina

--- app/view/jeu.view.php
+++ app/view/jeu.view.php
@@ -1,2 +1,9 @@
 <body>
-    <p>paragraphe</p>
+    <h1> Pixel Wars </h1>
+<div class= text>
+    <p>Faite nous donc vos plus beaux dessins, chaque mois nous reprendrons l'image et l'afficherons sur notre page d'accueil !</p>
+</div>
+<div class = img>
+    <img src="public/images/photos/pixelwars.jpg" alt= "pixelwars">
+</div>
+<button><a href="https://www.crazygames.fr/jeu/pixel-wars-of-hero">PLAY</a></button>
