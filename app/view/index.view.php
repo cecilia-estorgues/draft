cbc56be18faaf67f3dfe3cc6bade810421e7701c
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-27 08:49:41 +0200

    Caroussel accueil

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -29,9 +29,9 @@
 
         <div id="txt-t">
         <h2 class="txt2">Faites exploser vos couleurs</h2>
-                    <h3 class="txt3">Bienvenue chez nous, la micro-brasserie de Moissy-Cramayel</h3>
+                    <h3 class="txt3">Bienvenue chez nous, </br> la micro-brasserie de Moissy-Cramayel</h3>
         <p class="txt">Nous sommes une jeune équipe de 10 camarades, qui vient de créer ses propres bières originelles et atypiques, et qui
-                        à décider d'installer sa brasserie dans la ville de </br> Moissy-Cramayel dans le 77. Nous vous attendons nombreux avec votre
+                        à décider d'installer sa brasserie dans la ville de  Moissy-Cramayel dans le 77. Nous vous attendons nombreux avec votre
                         créativité et vos pensées positives pour venir gouter notre production.</p></div>
 
                         </section>
