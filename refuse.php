250fdf296dfffcfb71e1876b7b9e4a4c491c6987
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-27 11:22:32 +0200

    remodif

--- refuse.php
+++ refuse.php
@@ -10,9 +10,12 @@
 </head>
 <body>
 <main>
-<img src="public/images/Logo/lgo_bgnoir.png">
+<img src="public/images/Logo/lgo_bgnoir.png" id='im1'>
 <h2>Accès refusé</h2>
+<img src="public/images/icones/nobg/+18.png" id='im2'>
 <div><h4>Désolé mais tu n'as pas encore atteint l'âge requis pour accéder à notre site web. 
 Mais t'en fais pas, tu peux quand même chercher des infos sur <a href="http://www.responsibledrinking.org/?lang=fr">comment consommer de façon responsable.</a></h4></div>
+<p id='txt-ft'>L'abus d'alcool est dangereux pour la santé, à consommer avec modération !</p>
 </main>
 </body>
+</html>
