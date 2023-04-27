fb4954e54044362b61334cbad496330ba9979c7a
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-04-27 15:29:22 +0200

    nnn

--- app/view/actualites.view.php
+++ app/view/actualites.view.php
@@ -4,7 +4,7 @@
 
     <section>
         <div id="title">
-            <h1>NOTRE ACTUALITE</h1>
+            <h1>Notre actualité</h1>
         </div>
 
         <section id="contain2">
@@ -31,7 +31,7 @@
                     <img src="public/images/icones/nobg/calendrier.png">
                 </div>
                 <div id="droite2"><img rc="public/images/background/tache_orange.png">
-                    <p>Retrouvé nous donc le 7 juin, à Moissy Cramayel pour le festival tant attendu de la bière et découvrir la Butineuse et surtout notre marque Draft…</p>
+                    <p>Retrouvez nous donc le 7 juin, à Moissy Cramayel pour le festival tant attendu de la bière et découvrir la Butineuse et surtout notre marque Draft…</p>
                     <a href="#"> <button id="btn-2">En savoir plus</button> </a>
                 </div>
             </div>
