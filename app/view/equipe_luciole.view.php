e3063a489deef6c34cc7fc99ced9eb6e785133ff
Yotd28 <131147638+Yotd28@users.noreply.github.com>
2023-04-27 08:51:30 +0200

    modif luciole

--- app/view/equipe_luciole.view.php
+++ app/view/equipe_luciole.view.php
@@ -2,13 +2,13 @@
             <header></header>
             <div id="image-Lucioles">
         
-                <img src ="public/images/dessins/equip_luciole.png" alt = "Le personnage de luciole"> 
+                <img src ="public/images/dessins/equip_luciole.png" alt = "Le personnage de luciole"/>  
             </div>
             <div id="texte">
                 <h2>Luciole</h2>
                 <h3>Explorez</h3> 
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod </p>
-        
+    
             </div>
             <div id="boutton">
                 <p>La bière</p>
