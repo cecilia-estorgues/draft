708f49cff94f7ed362cb19690c3e25c3e544dae5
cecilia-estorgues <cecilia.estorgues@gmail.com>
2023-05-10 11:41:28 +0200

    jeu tache

--- app/view/jeu.view.php
+++ app/view/jeu.view.php
@@ -4,6 +4,6 @@
     <p>Faite nous donc vos plus beaux dessins, chaque mois nous reprendrons l'image et l'afficherons sur notre page d'accueil !</p>
 </div>
 <div class = img>
-    <img src="public/images/photos/pixelwars.jpg" alt= "pixelwars" id="img">
+    <img src="public/images/photos/pixelwars.jpg" alt= "image montrant un exemple de pixelwars, qui est un dessin fait de pixels" id="img">
 </div>
 <button><a href="https://www.crazygames.fr/jeu/pixel-wars-of-hero">PLAY</a></button>
