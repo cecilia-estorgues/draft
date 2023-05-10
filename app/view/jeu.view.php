792747d828ef3fef7f94b6eb272154280b8c355f
cecilia-estorgues <cecilia.estorgues@gmail.com>
2023-05-10 12:29:08 +0200

    jeu

--- app/view/jeu.view.php
+++ app/view/jeu.view.php
@@ -1,7 +1,7 @@
 <body>
     <h1> Pixel Wars </h1>
 <div class= text>
-    <p>Faite nous donc vos plus beaux dessins, chaque mois nous reprendrons l'image et l'afficherons sur notre page d'accueil !</p>
+    <p>Faites nous donc vos plus beaux dessins, chaque mois nous reprendrons l'image et l'afficherons sur notre page d'accueil !</p>
 </div>
 <div class = img>
     <img src="public/images/photos/pixelwars.jpg" alt= "image montrant un exemple de pixelwars, qui est un dessin fait de pixels" id="img">
