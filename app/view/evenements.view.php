3d0902a253c198d5ab55cd2b461012f5b896ca9d
Lilo-Yoan <yoaneraville@gmail.com>
2023-05-16 15:56:38 +0200

    Evenement

--- app/view/evenements.view.php
+++ app/view/evenements.view.php
@@ -1,7 +1,14 @@
 <main>
-
-
-
-
+<h1>Festival</h1> 
+ 
+<div id class= "back"><img src="public/images/background/FestiBack.png" alt="Festival Enzyme"></img></div>
+<div>  <img id class ="GB"src = "public/images/photos/GB.jpg" alt="Affiche Festival"></img>
+<h2>7 juin 2023</h2>    
+<p>Retrouvez nous au festival de la bière se déroulant le 7 juin à la  brasserie. 
+240 rue de la Motte, Moissy-Cramayel !
+Pendant ce festival, vous pourrez déguster notre nouvelle bière au miel de châtaignier. 
+Mais également exprimer votre créativité sur notre mur prévu à cet effet. 
+Et  les plus chanceux pourront même repartir avec certains de nos goodies. 
+D’autres surprise vous attendent alors  venez nombreux !</p></div>
 
 </main>
