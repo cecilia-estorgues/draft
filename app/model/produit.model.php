59c4bd2f60268db8afa99c54d6ed9f11cd31eb41
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-05-16 15:11:59 +0200

    db

--- app/model/produit.model.php
+++ app/model/produit.model.php
@@ -2,7 +2,7 @@
 
 function getProduit(PDO $dbco): array 
 {
-$sql="SELECT reference as ref, nom_produit FROM produit";
+$sql="SELECT nom_produit FROM produit";
  $query=$dbco->query($sql);
 $results=$query->fetchAll();
 
@@ -13,4 +13,21 @@ function convertType2Class(string $type): string
 {
     $str = strtolower($type);
     return preg_replace('/(é|è)/', 'e', $str);   
+}
+
+function Linknametoref(array $produits, PDO $dbco): array
+{
+
+    $sql = "SELECT nom_produit FROM produit";
+    $stmt = $dbco->prepare($sql);
+
+    $nos_produits = [];
+    foreach ($produits as $produit) {
+        $stmt->bindParam('nom_produit', $produit['reference'], PDO::PARAM_INT);
+        $stmt->execute();
+
+        $nos_produits[] = $produit;
+    }
+
+    return $nos_produits;
 }
