d4569a5e90fe69355b429f065eef568861dfe4b9
Max19647 <130546385+Max19647@users.noreply.github.com>
2023-05-23 12:16:01 +0200

    admin

---
+++ app/view/administration.view.php
@@ -0,0 +1,9 @@
+<main>
+
+<h1>Bonjour et bienvenue dans le système d'administration de la base de donnée, veuillez selectionner quelle donnée vous souhaitez ajouter.</h1>
+
+<ul>
+<a href="newclient.php"><li>Ajouter nouveau client</li> </a>
+<a href="newproduit.php"><li>Ajouter nouveau produit</li> </a>
+</ul>
+</main>
