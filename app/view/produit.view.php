a2dd002147f1f1d57f7d94db3aeec41a5c8b104d
AbelFEL <131149353+AbelFEL@users.noreply.github.com>
2023-05-23 10:56:31 +0200

    produits

--- app/view/produit.view.php
+++ app/view/produit.view.php
@@ -8,10 +8,23 @@
                     </a>
                 </figure>
                 <div class="description">
-                    <p class="pokemon-nom"><?= $biere['nom_produit'] ?></p>
+                    <p class="biere-nom"><?= $biere['nom_produit'] ?></p>
                 </div>
             </div>
         <?php endforeach ?>
-
+    </div>
+    <div class="container biere">
+        <?php foreach ($nos_goodies as $goodies) : ?>
+            <div class="biere">
+                <figure>
+                    <a href="">
+                        <img src="public/images/mockup/<?= $goodies['nom_produit'] ?>.png" alt="<?= $goodies['nom_produit'] ?>">
+                    </a>
+                </figure>
+                <div class="description">
+                    <p class="biere-nom"><?= $goodies['nom_produit'] ?></p>
+                </div>
+            </div>
+        <?php endforeach ?>
     </div>
 </main>
