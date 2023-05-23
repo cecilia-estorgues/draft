5e4b88b191373364062d20b36037c62f876fb610
cecilia-estorgues <cecilia.estorgues@gmail.com>
2023-05-23 12:25:17 +0200

    css new client fini

--- app/view/addnewclient.view.php
+++ app/view/addnewclient.view.php
@@ -1,31 +1,32 @@
 <main>
+<h1>Nouveau client</h1>
 <div class="container">
     <div class="message">
         
 </div>
     <form action="addnewclient.php" method="post">
-    <div>        <label for="nom">nom</label>
+    <div>        <label for="nom">Nom</label>
         <input type="text"  name="nom" aria-label="Nom du client" id="nom" required>
     </div>
-    <div>        <label for="prenom">prénom</label>
+    <div>        <label for="prenom">Prénom</label>
         <input type="text"  name="prenom" aria-label="Prénom du client" id="prenom" required>
     </div>
-    <div>        <label for="adresse">adresse</label>
+    <div>        <label for="adresse">Adresse</label>
         <input type="text"  name="adresse" aria-label="Adresse du client" id="adresse" required>
     </div>
-    <div>        <label for="code_postal">code postal</label>
+    <div>        <label for="code_postal">Code postal</label>
         <input type="number"  name="code_postal" aria-label="Code postal du client" id="code_postal" required>
     </div>
-    <div>        <label for="ville">ville</label>
+    <div>        <label for="ville">Ville</label>
         <input type="text"  name="ville" aria-label="Ville du client" id="ville" required>
     </div>
-    <div>        <label for="email">email</label>
+    <div>        <label for="email">Email</label>
         <input type="text"  name="email" aria-label="Mail du client" id="email" required>
     </div>
 
 
 
-        <input type="submit"  value="ajouter" aria-label="Ajouter un client">
+        <input class="button" type="submit"  value="ajouter" aria-label="Ajouter un client">
     </form>
 </div>
 </main>
