55f739d2fea9d1d047b48e7a110f3e0d6f89a7cd
Lilo-Yoan <yoaneraville@gmail.com>
2023-05-24 14:53:16 +0200

    Evenement modif

--- app/view/evenements.view.php
+++ app/view/evenements.view.php
@@ -3,8 +3,8 @@
  
 <div class = "back"><img src="public/images/background/FestiBack.png" alt="Festival Enzyme"></img></div>
 
-<div>  <img class  ="GB"src = "public/images/photos/GB.jpg" alt="Affiche Festival"></img>
-
+<div>  <img class  ="GB"src = "public/images/poster/GBposter.jpg" alt="Affiche Festival"></img>
+</div>
 <h2 class ="date">7 juin 2023</h2> 
 
 <p class ="RDV">Retrouvez nous au festival de la bière se déroulant le 7 juin à la  brasserie. 
@@ -12,6 +12,6 @@
 Pendant ce festival, vous pourrez déguster notre nouvelle bière au miel de châtaignier. 
 Mais également exprimer votre créativité sur notre mur prévu à cet effet. 
 Et  les plus chanceux pourront même repartir avec certains de nos goodies. 
-D’autres surprise vous attendent alors  venez nombreux !</p></div>
+D’autres surprise vous attendent alors  venez nombreux !</p>
 
 </main>
